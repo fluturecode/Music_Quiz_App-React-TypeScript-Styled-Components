3dd097f80767a7fe486d904422884007a787d561
Elizabeth Eidelson <fluturecode@gmail.com>
2020-12-02 16:32:53 -0500

    updated App.tsx and fixed TypeScript Version bug" "

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react"
+import QuestionCard from "./components/QuestionCard"
 
-function App() {
-	return <div className='App'>Quiz</div>
+const App = () => {
+	const startTrivia = async () => {}
+
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
+
+	const nextQuestion = () => {}
+
+	return (
+		<div className='App'>
+			<h1>Music Trivia</h1>
+			<button className='start' onClick={startTrivia}>
+				Start Game
+			</button>
+			<p className='score'>Score:</p>
+			<p>Loading Questions...</p>
+			<QuestionCard />
+			<button className='next' onClick={nextQuestion}>
+				Next Question
+			</button>
+		</div>
+	)
 }
 
 export default App
