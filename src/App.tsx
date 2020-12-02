7b408de15dd40967197744b5b6f74a069a2ef156
Elizabeth Eidelson <fluturecode@gmail.com>
2020-12-02 17:33:03 -0500

    added API.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
+import { fetchQuizQuestions } from "./API"
 import QuestionCard from "./components/QuestionCard"
+import { Difficulty } from "./API"
+
+const TOTAL_QUESTIONS = 10
 
 const App = () => {
+	const [loading, setLoading] = useState(false)
+	const [questions, setQuestions] = useState([])
+	const [number, setNumber] = useState(0)
+	const [userAnswers, setUserAnswers] = useState([])
+	const [score, setScore] = useState(0)
+	const [gameOver, setGameOver] = useState(true)
+
+	console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+
 	const startTrivia = async () => {}
 
 	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
@@ -14,9 +27,15 @@ const App = () => {
 			<button className='start' onClick={startTrivia}>
 				Start Game
 			</button>
-			<p className='score'>Score:</p>
-			<p>Loading Questions...</p>
-			<QuestionCard />
+			<p className='score'>Score:</p> <p>Loading Questions...</p>
+			{/* <QuestionCard
+				questionNr={number + 1}
+				totalQuestions={TOTAL_QUESTIONS}
+				question={questions[number].question}
+				answers={questions[number].answers}
+				userAnswer={userAnswers ? userAnswers[number] : undefined}
+				callback={checkAnswer}
+			/> */}
 			<button className='next' onClick={nextQuestion}>
 				Next Question
 			</button>
