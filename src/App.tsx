70a863df53fb610c7099dd816cc37bac96f863f6
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-06 00:34:49 -0500

    added checkAnswer function to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,24 @@ const App = () => {
 		setLoading(false)
 	}
 
-	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+		if (!gameOver) {
+			// Users answer
+			const answer = e.currentTarget.value
+			// Check answer against correct answer
+			const correct = questions[number].correct_answer === answer
+			// Add score if answer is correct
+			if (correct) setScore((prev) => prev + 1)
+			// Save answer in array for user answers
+			const answerObject = {
+				question: questions[number].question,
+				answer,
+				correct,
+				correctAnswer: questions[number].correct_answer,
+			}
+			setUserAnswers((prev) => [...prev, answerObject])
+		}
+	}
 
 	const nextQuestion = () => {}
 
@@ -59,9 +76,14 @@ const App = () => {
 					callback={checkAnswer}
 				/>
 			)}
-			<button className='next' onClick={nextQuestion}>
-				Next Question
-			</button>
+			{!gameOver &&
+			!loading &&
+			userAnswers.length === number + 1 &&
+			number !== TOTAL_QUESTIONS - 1 ? (
+				<button className='next' onClick={nextQuestion}>
+					Next Question
+				</button>
+			) : null}
 		</>
 	)
 }
